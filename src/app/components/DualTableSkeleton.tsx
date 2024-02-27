b4928e68988c79c13cd5ef0c26e6440e8290f427
Julio Xavier <jxarenas21990@gmail.com>
2024-02-27 13:05:36 -0500

    DEVTEMP-476 DEVTEMP-477

---
+++ src/app/components/DualTableSkeleton.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Skeleton, Card, Row, Col } from 'antd';
+
+const DualTableSkeleton = () => {
+    // Ajusta el número de filas según tus necesidades
+    const numberOfRows = 5;
+
+    return (
+        <Row gutter={16}>
+            {/* Dos columnas para las dos tablas */}
+            {[...Array(2)].map((_, colIndex) => (
+                <Col key={colIndex} span={12}>
+                    <Card>
+                        {Array.from({ length: numberOfRows }).map((_, rowIndex) => (
+                            <Skeleton key={rowIndex} loading={true} active>
+                                <Skeleton.Input style={{ width: '100%' }} active />
+                            </Skeleton>
+                        ))}
+                    </Card>
+                </Col>
+            ))}
+        </Row>
+    );
+};
+
+export default DualTableSkeleton;
